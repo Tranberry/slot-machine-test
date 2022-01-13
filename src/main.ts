a77d26a5e25feba8307792eb8a1398ac621cb4f8
TRBRY <oskar@tranberry.se>
2022-01-13 17:05:42 +0100

    only first symbols load

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { GM } from "./game_manager.js";
-import { makeCanvasImage } from "./reel_builder.js";
+import { _chooseReelSymbols, _makeCanvasImage } from "./new_reel_builder.js";
+// import { makeCanvasImage } from "./reel_builder.js";
 import { ReelSymbolImages } from "./slot_symbols.js";
 // import { GameMNG } from "./testing_classes.js";
 
@@ -17,8 +18,35 @@ GM.GAMEMANAGER.spinReels();
 
 // *****************************************************************************
 
-makeCanvasImage().then((canvas) => {
-  const dataURL = canvas.toDataURL("image/png");
+// makeCanvasImage().then((canvas) => {
+//   const dataURL = canvas.toDataURL("image/png");
+
+//   const testImg = document.createElement("img");
+//   const reelDiv = document.querySelector(".reel") as HTMLDivElement;
+//   testImg.src = dataURL;
+//   testImg.id = "testImg";
+
+//   if (reelDiv) {
+//     // set url to css variable
+//     // document.body.style.setProperty("--reel-image", `url(${dataURL})`);
+//     // set background to --reel-image
+//     reelDiv.style.backgroundImage = `url(${dataURL})`;
+//   }
+
+//   document.body.appendChild(testImg);
+// });
+
+// *****************************************************************************
+
+const choiceOne = _chooseReelSymbols(ReelSymbolImages, 11, true);
+
+// const choiceTwo = _chooseReelSymbols(ReelSymbolImages, 11, false);
+
+// console.log(choiceOne);
+// console.log(choiceTwo);
+
+_makeCanvasImage(choiceOne).then((canvas) => {
+  const dataURL = canvas;
 
   const testImg = document.createElement("img");
   const reelDiv = document.querySelector(".reel") as HTMLDivElement;
